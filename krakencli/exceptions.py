c5d46a169c9fff2fc57df27ed3990661da471d19
Patrick McQuay <patrick.mcquay@gmail.com>
2021-01-13 15:36:07 -0500

    Make pre-commit fixes

--- krakencli/exceptions.py
+++ krakencli/exceptions.py
@@ -3,4 +3,5 @@ class InvalidKeyFile(Exception):
 
     def __init__(self, file_path):
         self.file_path = file_path
-        super().__init__(f"Key file {self.file_path} is not formatted properly")
+        super().__init__(f"Key file {self.file_path} is"
+                         f"not formatted properly")
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="kraken_cli", # Replace with your own username
+    name="krakencli",  # Replace with your own username
     version="0.0.1",
     author="Patrick McQuay",
     author_email="patrick.mcquay@gmail.com",
