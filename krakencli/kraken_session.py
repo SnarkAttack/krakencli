51522e7057e8371553e098c40dcf46f8d3910966
Patrick McQuay <patrick.mcquay@gmail.com>
2021-01-13 12:57:07 -0500

    Create barebones KrakenSession initialization

---
+++ krakencli/kraken_session.py
@@ -0,0 +1,29 @@
+
+
+DEFAULT_KRAKEN_API_DOMAIN = "https://api.kraken.com"
+DEFAULT_KRAKEN_API_VERSION = 0
+DEFAULT_KRAKEN_API_PUBLIC_ADDRESS = "public"
+DEFAULT_KRAKEN_API_PRIVATE_ADDRESS = "private"
+
+class KrakenSession(object):
+
+    def __init__(self,
+                 api_domain=DEFAULT_KRAKEN_API_DOMAIN,
+                 api_version=DEFAULT_KRAKEN_API_VERSION,
+                 api_key=None,
+                 private_key=None
+                ):
+        self._api_domain = api_domain
+        self._api_version = api_version
+        self._api_key = api_key
+        self._private_key = private_key
+
+    def set_api_key(self, api_key):
+        self._api_key = api_key
+
+    def set_private_key(self, private_key):
+        self._private_key = private_key
+
+    def load_keys_from_file(self, file_path):
+        with open(file_path, "r") as f:
+            print
