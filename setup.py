229ae260273a97553663feaf85b300c2b80a478c
Patrick McQuay <patrick.mcquay@gmail.com>
2021-01-13 12:59:11 -0500

    Update url in setup.py for package

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Python3 API for Kraken cryptocurrency trading site",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/SnarkAttack/krakencli",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
